8d1367e71d578ee37b09386b3a10056a1c108643
Phillip-08 <c.phillip.07@gmail.com>
2022-10-05 22:58:46 -0300

    Cambiois

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = "restaurante.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
